ca4e1b0101e53b67209bae0f92ca4b95359d9002
KenM4sters <EHCKenway@outlook.com>
2024-02-09 17:25:52 +0000

    init :zap:

---
+++ src/main/java/com/characterselection/characterselection/CharacterselectionApplication.java
@@ -0,0 +1,13 @@
+package com.characterselection.characterselection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterselectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterselectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/characterselection/characterselection/CharacterselectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.characterselection.characterselection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterselectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
